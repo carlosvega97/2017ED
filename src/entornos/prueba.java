b88795ae75709127c5ea81b9cea761fd88d839b7
carlo <carlo@LAPTOP-HBCJE6EN>
2017-01-30 16:57:52 +0100

    segunda version

---
+++ src/entornos/prueba.java
@@ -0,0 +1,21 @@
+package entornos;
+
+public class prueba {
+	
+	static java.util.Scanner in;
+
+	public static void casoDePrueba() {
+		int n = in.nextInt();
+		int m = in.nextInt();
+		int resultado = n* m * 4;
+		System.out.println(resultado);
+	}
+	public static void main(String args[]) {
+		in = new java.util.Scanner(System.in);
+		int numCasos;
+		numCasos = Integer.parseInt(in.nextLine());
+		for (int i = 0; i < numCasos; i++)
+		casoDePrueba();
+		}
+			
+	}
