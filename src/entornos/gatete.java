e2bc95c353d7ac1197ee7f0a06b298b96f636ee7
carlo <carlo@LAPTOP-HBCJE6EN>
2017-02-06 16:32:47 +0100

    por probar

--- src/entornos/gatete.java
+++ src/entornos/gatete.java
@@ -1,5 +1,15 @@
 package entornos;
 
 public class gatete {
+	int numero_vidas;
+
+	public int getNumero_vidas() {
+		return numero_vidas;
+	}
+
+	public void setNumero_vidas(int numero_vidas) {
+		this.numero_vidas = numero_vidas;
+	}
+	
 
 }
